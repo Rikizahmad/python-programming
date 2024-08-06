2733d4fa17b1124a885cd1e2aec292a4412da8bc
Rikizahmad <bagjar150@gmail.com>
2024-08-06 09:41:56 +0700

    Add files via upload

---
+++ pertemuan 5/membership.py
@@ -0,0 +1,7 @@
+makanan = ["kuen pancong", "nasi goreng", "burger"]
+
+print("Apakah nasi uduk merupakan anggota?" "nasi uduk" in makanan)
+print("apakah burger merupakan anggota?", "burger" in makanan)
+
+print("Apakah nasi uduk merupakan anggota?" "nasi uduk" not in makanan)
+print("apakah burger merupakan anggota?", "burger" not in makanan)
